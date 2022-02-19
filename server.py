5f8f86099b45d6c9f0281d03cbaaa17f893a26c5
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2022-02-19 12:22:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,4 +3,5 @@ import threading
 #threading is an essential for creating multiple thread
 
 PORT = 5050
-SERVER = "192.168.56.1"
+SERVER = socket.gethostbyname(socket.gethostname())
+#SERVER = "192.168.56.1"
