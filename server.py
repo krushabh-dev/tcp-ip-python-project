94b15da635067504f2f947a9cadca4ace9d177d1
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2022-02-19 14:38:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+from concurrent.futures import thread
 import socket
+from sqlite3 import connect
 import threading
 #threading is an essential for creating multiple thread
 
@@ -13,3 +15,19 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #binding to a address
 server.bind(ADDR)
 
+
+def handel_client(conn, addr):
+    print(f"=> NEW CONNECTIONS {addr} connected.")
+    connected = True
+
+    while connected:
+        msg = conn.recv(64)
+
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handel_client, args={conn, addr})
+        thread.start()  # starting
+        print(f"=> ACTIVE CONNECTIONS {threading.activeCount() - 1 }")
